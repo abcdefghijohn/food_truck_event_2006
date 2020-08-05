6d4d80ece906b2cc9ed57a44c3c24f0b9027477d
John Kim <abcdefghijohnk@gmail.com>
2020-08-05 09:04:30 -0600

    create item exists and has attributes test

---
+++ test/food_truck_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+require './lib/food_truck'
+
+class FoodTruckTest < Minitest::Test
+
+  def test_it_exists_and_has_attributes
+    
+
+  end
+end
