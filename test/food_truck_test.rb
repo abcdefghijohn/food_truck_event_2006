61efefeef5d4540cdda99dc643e2c6cd6c7dd469
John Kim <abcdefghijohnk@gmail.com>
2020-08-05 09:15:34 -0600

    food truck exist and attributes test pass

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -11,6 +11,5 @@ class FoodTruckTest < Minitest::Test
     assert_instance_of FoodTruck, food_truck
     assert_equal "Rocky Mountain Pies", food_truck.name
     assert_equal [], food_truck.inventory
-    assert_equal 0, food_truck.check_stock(item1)
   end
 end
