73042cf68c6dacab7c5ba3ff79ad553846164105
John Kim <abcdefghijohnk@gmail.com>
2020-08-05 09:12:58 -0600

    wrote food truck exists and attributes test

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -6,7 +6,11 @@ require './lib/food_truck'
 class FoodTruckTest < Minitest::Test
 
   def test_it_exists_and_has_attributes
-    
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
 
+    assert_instance_of FoodTruck, food_truck
+    assert_equal "Rocky Mountain Pies", food_truck.name
+    assert_equal [], food_truck.inventory
+    assert_equal 0, food_truck.check_stock(item1)
   end
 end
